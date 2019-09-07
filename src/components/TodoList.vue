de2dc6f73da1276ba839e9fa4dd69dd3639b1aa2
knowhere1998 <darekar.amey@gmail.com>
2019-09-07 13:11:44 +0530

    Init Project

---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		TodoList goes here
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "TodoList",
+		data () {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
